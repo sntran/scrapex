82c924e6870aca6ee389443c153d4538e609d207
Son Tran-Nguyen <esente@gmail.com>
2015-10-03 18:05:58 -0500

    Test for extracting text content

--- test/scrapex/selector_test.exs
+++ test/scrapex/selector_test.exs
@@ -9,4 +9,12 @@ defmodule Scrapex.SelectorTest do
 
     assert href === ["http://www.iana.org/domains/example"]
   end
+
+  test "get text content" do
+    href = HTTPoison.get!("http://localhost:9090/example.com.html").body
+    |> Selector.select("h1")
+    |> Selector.extract("text")
+
+    assert href === ["Example Domain"]
+  end
 end
